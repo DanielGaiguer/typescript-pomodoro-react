0cc28d61aca39589f95555da0f86b93789dc35b4
Daniel Gaiguer <gaiguer2019@gmail.com>
2025-12-23 21:01:28 -0300

    Fix bug

--- src/utils/seconds-to-time.ts
+++ src/utils/seconds-to-time.ts
@@ -1,6 +1,6 @@
 import { zeroLeft } from './zeroLeft';
 
-export function secondsToMinutes(seconds: number): string {
+export function secondsToTime(seconds: number): string {
   const hours = zeroLeft(seconds / 3600);
   const min = zeroLeft((seconds / 60) % 60);
   const sec = zeroLeft((seconds % 60) % 60);
